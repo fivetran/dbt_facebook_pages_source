d14283d76cc7c0de1df0118783d1b04bf5d98f87
Dylan Baker <dylan@dbanalytics.co>
2021-11-02 19:26:08 +0000

    fix partition

--- models/stg_facebook_pages__post_history.sql
+++ models/stg_facebook_pages__post_history.sql
@@ -60,7 +60,7 @@ final as (
 
     select
         *,
-        row_number() over (partition by post_id order by _fivetran_synced desc) = 1 as is_most_recent_record
+        row_number() over (partition by post_id, source_relation order by _fivetran_synced desc) = 1 as is_most_recent_record
     from final
 
 )
